7ab09703a411809638df6796de19c8ae9308f77b
Oana <oana.demian@gmail.com>
2023-11-13 00:26:21 +0000

    enables dark mode for the app's background

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Stack } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 import Box from '@mui/material/Box';
 import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline'
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
 import { RewardGiver } from './components/RewardGiver';
@@ -14,30 +15,33 @@ function MyApp() {
   const colorMode = useContext(ColorModeContext);
 
   return (
-    <Box
-      sx={{
-        display:'flex',
-        justifyContent:'center',
-        alignItems:'center',
-        minHeight: '100vh',
-        ml: 'auto',
-        mr: 'auto',
-        bgcolor: 'background.default',
-        color: 'text.primary',
-        borderRadius: 1,
-        p: 3,
-      }}
-    >
-      <Stack spacing={10}>
-        <Stack direction="row">
-          {theme.palette.mode} mode
-          <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
-              {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
-          </IconButton>
-        </Stack>
-        <RewardGiver />
-      </Stack>
-    </Box>
+    <>
+      <div>
+          <div style={{width:'100%', position:'relative'}}>
+            <IconButton sx={{ ml: 1, position:'absolute', right:'100%', transform:'translate(100vw, 0)' }} onClick={colorMode.toggleColorMode} color="inherit">
+                {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+            </IconButton>
+          </div>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent:'center',
+              alignItems:'center',
+              minHeight: '100vh',
+              ml: 'auto',
+              mr: 'auto',
+              borderRadius: 1,
+              p: 3,
+            }}
+          >
+             <Stack spacing={10}>
+              <Stack direction="row">
+              </Stack>
+              <RewardGiver />
+            </Stack>
+            </Box>
+        </div>
+      </>
   );
 }
 
@@ -65,7 +69,9 @@ const ToggleColorMode = () => {
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
-        <MyApp />
+        <CssBaseline >
+          <MyApp />
+          </CssBaseline>
       </ThemeProvider>
     </ColorModeContext.Provider>
   );
